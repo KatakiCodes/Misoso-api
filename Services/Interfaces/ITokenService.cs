49f02d534e743fb66b5c7e4389b2814bf36952f7
Nelson Dos Santos <katakicode001@gmail.com>
2025-08-14 12:48:11 +0100

    Revert "Completado os cruds e endpoints"
    This reverts commit e30cab7e7c7e5dcb773510abadc15040e2ef9a21.

--- Services/Interfaces/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Misoso.Api.DTOs.Responses;
-
-namespace Misoso.Api.Services.Interfaces;
-
-public interface ITokenService
-{
-    public string GenerateToken(UserResponse userResponse);
-}
