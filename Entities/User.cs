902ac908bbcf67cc45d9ac0449b7c2e88552b259
Nelson Dos Santos <katakicode001@gmail.com>
2025-09-03 12:37:36 +0100

    feat: Including externalId field in userContext

--- Entities/User.cs
+++ Entities/User.cs
@@ -7,20 +7,23 @@ namespace Misoso.api.Entities
     {
         public User()
         {}
-        public User(string email, string userName, string password)
+        public User(string email, string userName, string? password = default, string? externalId = default)
         {
             Email = email;
             UserName = userName;
             Password = password;
+            ExternalId = externalId;
         }
-        public User(int id,string email, string userName, string password) : base(id)
+        public User(int id,string email, string userName, string? password = default, string? externalId = default) : base(id)
         {
             Email = email;
             UserName = userName;
             Password = password;
+            ExternalId = externalId;
         }
         public string Email { get; private set; }
         public string UserName { get; private set; }
-        public string Password { get; private set; }
+        public string? Password { get; private set; }
+        public string? ExternalId { get; private set; }
     }
 }
---
+++ Migrations/20250903112934_Including_External_Id_in_user_table.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Misoso.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Including_External_Id_in_user_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
